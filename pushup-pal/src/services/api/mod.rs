4d3334efe160d61e82fdd7fadd2c2bf14afd5f4e
Jacob <jacob@notactivelylooking.com>
2024-07-16 20:01:32 +0100

    setup api configs.

---
+++ pushup-pal/src/services/api/mod.rs
@@ -0,0 +1,11 @@
+mod pushups;
+
+use actix_web::web;
+
+pub fn pushup_scope(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/pushup")
+            .route("", web::get().to(pushups::get))
+            .route("/<session_id>", web::get().to(pushups::get))
+    );
+}
