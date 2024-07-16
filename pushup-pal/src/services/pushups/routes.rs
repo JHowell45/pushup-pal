de8ba96ce5a7ab5324f43c73f6c818494abb9f41
Jacob <jacob@notactivelylooking.com>
2024-07-16 19:24:23 +0100

    should be public.

--- pushup-pal/src/services/pushups/routes.rs
+++ pushup-pal/src/services/pushups/routes.rs
@@ -1,6 +1,6 @@
 use actix_web::{HttpResponse, Responder, Result};
 use std::collections::HashMap;
 
-async fn get() -> Result<impl Responder> {
+pub async fn get() -> Result<impl Responder> {
     Ok(HttpResponse::Ok().json(HashMap::from([("hello", "Test")])))
 }
