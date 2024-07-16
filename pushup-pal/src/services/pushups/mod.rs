96162a03cbeb219013371055a60a48b540da6fc4
Jacob <jacob@notactivelylooking.com>
2024-07-16 20:01:25 +0100

    removing old modules.

--- pushup-pal/src/services/pushups/mod.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::web;
-
-mod routes;
-
-use routes::get;
-
-pub fn pushup_scope(cfg: &mut web::ServiceConfig) {
-    cfg.route("", web::get().to(get));
-}
--- pushup-pal/src/services/pushups/routes.rs
+++
@@ -1,6 +0,0 @@
-use actix_web::{HttpResponse, Responder, Result};
-use std::collections::HashMap;
-
-pub async fn get() -> Result<impl Responder> {
-    Ok(HttpResponse::Ok().json(HashMap::from([("hello", "Test")])))
-}
